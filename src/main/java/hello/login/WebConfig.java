34a4e80af387fc8326a04d3c284d8cc279a564a7
MOONJJJJJ <yangmoonjj5@gmail.com>
2024-01-22 18:02:01 +0900

    7 - 스프링 인터셉터 - 인증 체크

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(2)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico",  "/error");
     }
     //@Bean
     public FilterRegistrationBean logFilter() {
